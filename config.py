7063f56638c78c5b20b239ed50aa2bad80e74fbd
bartoj35 <barton.jakub1811@gmail.com>
2022-01-23 19:09:05 +0100

    Add KOS alarm with template config

---
+++ config.py
@@ -0,0 +1,5 @@
+HOST=""
+USERNAME=""
+PASSWORD=""
+SOUND=""
+SUBJECT=""
---
+++ main.py
@@ -0,0 +1,27 @@
+from imap_tools import MailBox, AND
+from playsound import playsound
+from config import *
+from datetime import date
+
+def ring ( ):
+	while True:
+		playsound ( SOUND, block = True )
+
+# login to mail inbox
+mailbox = MailBox ( HOST )
+mailbox . login ( USERNAME, PASSWORD, "INBOX" )
+
+while ( True ):
+	# get mails date with subject 
+	mails = [ msg . date for msg in mailbox . fetch ( AND ( subject = SUBJECT ) ) ]
+	
+	# have mail 
+	if len ( mails ) == 0:
+		continue
+
+	# check if it is new mail
+	if mails [ 0 ] . strftime ( "%Y-%m-%d" ) . startswith ( date . today ( ) . strftime ( "%Y-%m-%d" ) ):
+		ring ( )
+
+mailbox.logout()
+
